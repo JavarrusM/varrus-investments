fd80bff41732c205fe7ac7e12da13386c37c0ffc
SolstarterVarrus <31835865+JavarrusM@users.noreply.github.com>
2022-06-18 18:54:34 -0400

    create: basic navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+  Flex,
+  Box,
+  Spacer,
+} from "@chakra-ui/react";
+import { FcMenu, FcHome, FcAbout } from "react-icons/fc";
+import { BsSearch } from "react-icons/bs";
+import { FiKey } from "react-icons/fi";
+
+const Navbar = () => (
+  <Flex p="2" boardBottom="1px" borderColor="gray.100">
+    <Box fontSize="3x1" color="blue.400" fontWeight="bold">
+      <Link href="/" paddingLeft="2">
+        Varrus
+      </Link>
+    </Box>
+    <Spacer />
+    <Box>
+      <Menu>
+        <MenuButton
+          as={IconButton}
+          icon={<FcMenu />}
+          variant="outline"
+          color="red.100"
+        />
+        <MenuList>
+          <Link href="/" passHref>
+            <MenuItem icon={<FcHome />}>Home</MenuItem>
+          </Link>
+          <Link href="/search" passHref>
+            <MenuItem icon={<BsSearch />}>Search</MenuItem>
+          </Link>
+          <Link href="/search?purpose-for-sale" passHref>
+            <MenuItem icon={<FcAbout />}>Buy Property</MenuItem>
+          </Link>
+          <Link href="/search?purpose-for-rent" passHref>
+            <MenuItem icon={<FcAbout />}>Rent Property</MenuItem>
+          </Link>
+        </MenuList>
+      </Menu>
+    </Box>
+  </Flex>
+);
+
+export default Navbar;
