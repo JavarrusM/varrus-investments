30f37bc19ed47a5b215a69303889fc0a8f7ae34f
SolstarterVarrus <31835865+JavarrusM@users.noreply.github.com>
2022-06-18 19:05:00 -0400

    fix: navbar component attribute

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,7 +14,7 @@ import { BsSearch } from "react-icons/bs";
 import { FiKey } from "react-icons/fi";
 
 const Navbar = () => (
-  <Flex p="2" boardBottom="1px" borderColor="gray.100">
+  <Flex p="2" borderBottom="1px" borderColor="gray.100">
     <Box fontSize="3x1" color="blue.400" fontWeight="bold">
       <Link href="/" paddingLeft="2">
         Varrus
