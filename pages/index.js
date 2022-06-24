920baa811c3b26eee57a4f2957a99dc2c47c86af
SolstarterVarrus <31835865+JavarrusM@users.noreply.github.com>
2022-06-24 18:11:30 -0400

    update: adding hero to index

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
 import Property from "../components/Property";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
+import Hero from "../components/Hero";
 
 const Banner = ({
   purpose,
@@ -46,38 +47,39 @@ const Banner = ({
 
 export default function Home({ propertiesForSale, propertiesForRent }) {
   return (
-    <Box>
-      <Banner
-        purpose="RENT A HOME"
-        title1="Rental Homes for"
-        title2="Everyone"
-        desc1="Explore Apartments, Villas, Homes"
-        desc2="and more"
-        buttonText="Explore Renting"
-        linkName="/search?purpose=for-rent"
-        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
-      />
-      <Flex flexWrap="wrap" gridGap={2}>
-        {propertiesForRent.map((property) => (
-          <Property property={property} key={property.id} />
-        ))}
-      </Flex>
-      <Banner
-        purpose="BUY A HOME"
-        title1="Find, Buy & Own Your"
-        title2="Dream Home"
-        desc1="Explore Apartments, Villas, Homes"
-        desc2="and more"
-        buttonText="Explore Buying"
-        linkName="/search?purpose=for-sale"
-        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
-      />
-      <Flex flexWrap="wrap" gridGap={2}>
-        {propertiesForSale.map((property) => (
-          <Property property={property} key={property.id} />
-        ))}
-      </Flex>
-    </Box>
+    <Hero />
+    // <Box>
+    //   <Banner
+    //     purpose="RENT A HOME"
+    //     title1="Rental Homes for"
+    //     title2="Everyone"
+    //     desc1="Explore Apartments, Villas, Homes"
+    //     desc2="and more"
+    //     buttonText="Explore Renting"
+    //     linkName="/search?purpose=for-rent"
+    //     imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+    //   />
+    //   <Flex flexWrap="wrap" gridGap={2}>
+    //     {propertiesForRent.map((property) => (
+    //       <Property property={property} key={property.id} />
+    //     ))}
+    //   </Flex>
+    //   <Banner
+    //     purpose="BUY A HOME"
+    //     title1="Find, Buy & Own Your"
+    //     title2="Dream Home"
+    //     desc1="Explore Apartments, Villas, Homes"
+    //     desc2="and more"
+    //     buttonText="Explore Buying"
+    //     linkName="/search?purpose=for-sale"
+    //     imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+    //   />
+    //   <Flex flexWrap="wrap" gridGap={2}>
+    //     {propertiesForSale.map((property) => (
+    //       <Property property={property} key={property.id} />
+    //     ))}
+    //   </Flex>
+    // </Box>
   );
 }
 
