77913979aa9cc90f0577dad0cc0f6ab19a889dad
SolstarterVarrus <31835865+JavarrusM@users.noreply.github.com>
2022-06-28 14:46:23 -0400

    update: image scrolling for firebase data

--- components/ImageScrollBar.jsx
+++ components/ImageScrollBar.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import Image from "next/image";
-import { Box, Icon, Flex } from "@chakra-ui/react";
+import { Box, Icon, Flex, Text } from "@chakra-ui/react";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
@@ -40,19 +40,23 @@ const ImageScrollBar = ({ data }) => (
     RightArrow={RightArrow}
     style={{ overflow: "hidden" }}
   >
-    {data.map((item) => (
-      <Box key={item.id} width="910px" itemId={item.id} overflow="hidden" p="1">
-        <Image
-          alt="property"
-          placeholder="blur"
-          blurDataURL={item.url}
-          src={item.url}
-          width={1000}
-          height={500}
-          sizes="(max-width:500px) 100px, (max-width):1023px 400px, 1000px"
-        />
-      </Box>
-    ))}
+    {data.map((item, idx) => {
+      console.log(item);
+      return (
+        <Box key={idx} width="910px" itemID={idx} overflow="hidden" p="1" >
+          <Image
+            alt="property"
+            placeholder="blur"
+            blurDataURL={item.url}
+            src={item.url}
+            width={1000}
+            height={500}
+            sizes="(max-width:500px) 100px, (max-width):1023px 400px, 1000px"
+          />
+          {item.description && <Text>{item.description}</Text>}
+        </Box>
+      );
+    })}
   </ScrollMenu>
 );
 
