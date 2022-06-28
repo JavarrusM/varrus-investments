3fb2881afe0da76011bdcf45773a6f5e517f4fb7
SolstarterVarrus <31835865+JavarrusM@users.noreply.github.com>
2022-06-28 14:44:41 -0400

    update: next config for firebase images

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
-  }
+    domains: ["firebasestorage.googleapis.com"],
+  },
 };
 
 module.exports = nextConfig;
