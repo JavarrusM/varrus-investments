fd7fdfd5fa08f6445104dc545b26484f5ebc14cd
SolstarterVarrus <31835865+JavarrusM@users.noreply.github.com>
2022-06-29 14:07:54 -0400

    update: property detail image view and firebase integration

--- components/ImageScrollBar.jsx
+++ components/ImageScrollBar.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import Image from "next/image";
-import { Box, Icon, Flex, Text } from "@chakra-ui/react";
+import { Box, Icon, Flex, Text, Button } from "@chakra-ui/react";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
@@ -35,29 +35,69 @@ const RightArrow = () => {
 };
 
 const ImageScrollBar = ({ data }) => (
-  <ScrollMenu
-    LeftArrow={LeftArrow}
-    RightArrow={RightArrow}
-    style={{ overflow: "hidden" }}
-  >
-    {data.map((item, idx) => {
-      console.log(item);
-      return (
-        <Box key={idx} width="910px" itemID={idx} overflow="hidden" p="1" >
-          <Image
-            alt="property"
-            placeholder="blur"
-            blurDataURL={item.url}
-            src={item.url}
-            width={1000}
-            height={500}
-            sizes="(max-width:500px) 100px, (max-width):1023px 400px, 1000px"
-          />
-          {item.description && <Text>{item.description}</Text>}
-        </Box>
-      );
-    })}
-  </ScrollMenu>
+  <>
+    {/* <ScrollMenu
+      LeftArrow={LeftArrow}
+      RightArrow={RightArrow}
+      style={{ overflow: "hidden" }}
+    > */}
+      {data.map((item, idx) => {
+        return (
+          <Box as={item.type} src={item.url} key={idx} width="full" height="75vh" itemID={idx}>
+            {/* {item.tpe === "photo" && (
+              <Image
+                alt="property"
+                placeholder="blur"
+                blurDataURL={item.url}
+                src={item.url}
+                width={1000}
+                height={500}
+                sizes="(max-width:500px) 100px, (max-width):1023px 400px, 1000px"
+              />
+            )}
+            {item.tpe === "3d" && (
+              <iframe
+                width="100%"
+                height="640"
+                frameBorder="0"
+                allowFullScreen
+                allow="xr-spatial-tracking; gyroscope; accelerometer"
+                scrolling="no"
+                src={item.url}
+              />
+            )}
+            {item.tpe === "3d" && (
+              <iframe
+                width="100%"
+                height="640"
+                frameBorder="0"
+                allowFullScreen
+                allow="xr-spatial-tracking; gyroscope; accelerometer"
+                scrolling="no"
+                src={item.url}
+              />
+            )} */}
+          </Box>
+        );
+      })}
+    {/* </ScrollMenu> */}
+    {/* <span> */}
+    <Flex justify={"end"}>
+      <Text as="cite" fontSize={"sm"} textAlign="end">
+        Powered by Varrus Services Media{" "}
+        <Button
+          cursor="pointer"
+          as="cite"
+          variant={"link"}
+          colorScheme={"blue"}
+          size={"sm"}
+        >
+          Learn more
+        </Button>
+      </Text>
+    </Flex>
+    {/* </span> */}
+  </>
 );
 
 export default ImageScrollBar;
