53a4085645835ea46c2f420d7d07054bfcfd8fbe
SolstarterVarrus <31835865+JavarrusM@users.noreply.github.com>
2022-06-29 16:12:55 -0400

    refactor: clean up index

--- pages/index.js
+++ pages/index.js
@@ -6,105 +6,10 @@ import Property from "../components/Property";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 import Hero from "../components/Hero";
 
-const Banner = ({
-  purpose,
-  title1,
-  title2,
-  desc1,
-  desc2,
-  buttonText,
-  linkName,
-  imageUrl,
-}) => (
-  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-    <Image src={imageUrl} width={500} height={500} alt="banner" />
-    <Box p="5">
-      <Text color="gray.500" fontSize="sm" fontWeight="medium">
-        {purpose}
-      </Text>
-      <Text fontSize="3xl" fontWeight="bold">
-        {title1}
-        <br />
-        {title2}
-      </Text>
-      <Text
-        color="gray.700"
-        fontSize="lg"
-        fontWeight="medium"
-        paddingTop="3"
-        paddingBottom="3"
-      >
-        {desc1}
-        <br />
-        {desc2}
-      </Text>
-      <Button fontSize="xl">
-        <Link href={linkName}>{buttonText}</Link>
-      </Button>
-    </Box>
-  </Flex>
-);
-
-export default function Home({ propertiesForSale, propertiesForRent }) {
+export default function Home() {
   return (
     <>
       <Hero />
-      <iframe
-        width="853"
-        height="480"
-        src="https://my.matterport.com/show/?m=78JiM1dQoCe"
-        frameBorder="0"
-        allowFullScreen
-        allow="xr-spatial-tracking"
-      ></iframe>
     </>
-    // <Box>
-    //   <Banner
-    //     purpose="RENT A HOME"
-    //     title1="Rental Homes for"
-    //     title2="Everyone"
-    //     desc1="Explore Apartments, Villas, Homes"
-    //     desc2="and more"
-    //     buttonText="Explore Renting"
-    //     linkName="/search?purpose=for-rent"
-    //     imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
-    //   />
-    //   <Flex flexWrap="wrap" gridGap={2}>
-    //     {propertiesForRent.map((property) => (
-    //       <Property property={property} key={property.id} />
-    //     ))}
-    //   </Flex>
-    //   <Banner
-    //     purpose="BUY A HOME"
-    //     title1="Find, Buy & Own Your"
-    //     title2="Dream Home"
-    //     desc1="Explore Apartments, Villas, Homes"
-    //     desc2="and more"
-    //     buttonText="Explore Buying"
-    //     linkName="/search?purpose=for-sale"
-    //     imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
-    //   />
-    //   <Flex flexWrap="wrap" gridGap={2}>
-    //     {propertiesForSale.map((property) => (
-    //       <Property property={property} key={property.id} />
-    //     ))}
-    //   </Flex>
-    // </Box>
   );
-}
-
-export async function getStaticProps() {
-  const propertyForSale = await fetchApi(
-    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`
-  );
-  const propertyForRent = await fetchApi(
-    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`
-  );
-
-  return {
-    props: {
-      propertiesForSale: propertyForSale?.hits,
-      propertiesForRent: propertyForRent?.hits,
-    },
-  };
 }
