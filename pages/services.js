a064b83da872495c29ff8d8fbda5ed4f4dbb3b7f
SolstarterVarrus <31835865+JavarrusM@users.noreply.github.com>
2022-08-05 14:14:56 -0400

    update: refactor services page

--- pages/services.js
+++ pages/services.js
@@ -21,50 +21,59 @@ import {
 } from "firebase/firestore";
 import Service from "../components/Service";
 
-const SERVICES = {
-  RealEstate: [
-    {
-      name: "Real Estate Sales",
-      ref: "/selling",
-      cover:
-        "https://firebasestorage.googleapis.com/v0/b/varrus-ee20f.appspot.com/o/services%2Fvarrus-properties-realtor-block.png?alt=media&token=8a9a5fbf-096b-44a1-bde5-ff163dfb72f8",
-    },
-    {
-      name: "Property Management",
-      ref: "/leasing",
-      cover:
-        "https://firebasestorage.googleapis.com/v0/b/varrus-ee20f.appspot.com/o/services%2Fvarrus-properties-management-block.png?alt=media&token=80fafa7f-ea41-4de1-9561-ccc6ad88d9eb",
-    },
-  ],
-  Media: [
-    {
-      name: "Drone Filmography",
-      ref: "/drone",
-      cover:
-        "https://firebasestorage.googleapis.com/v0/b/varrus-ee20f.appspot.com/o/services%2Fvarrus-media-drone-block.png?alt=media&token=39ca4025-6477-4f59-b044-c208ee29702c",
-    },
-    {
-      name: "Virtual Photography",
-      ref: "/virtual",
-      cover:
-        "https://firebasestorage.googleapis.com/v0/b/varrus-ee20f.appspot.com/o/services%2Fvarrus-media-photo-block.png?alt=media&token=0184a882-4dac-4133-9b63-4b695e81820e",
-    },
-  ],
-  Construction: [
-    {
-      name: "General Construction",
-      ref: "/general",
-      cover:
-        "https://firebasestorage.googleapis.com/v0/b/varrus-ee20f.appspot.com/o/services%2Fvarrus-construction-general-block.png?alt=media&token=76c39f6e-5200-4a27-824b-5266515d3d81",
-    },
-    {
-      name: "Renovations",
-      ref: "/renovations",
-      cover:
-        "https://firebasestorage.googleapis.com/v0/b/varrus-ee20f.appspot.com/o/services%2Fvarrus-construction-renovations-block.png?alt=media&token=cf3d9c83-48cb-4e22-8caf-a3dc43710130",
-    },
-  ],
-};
+const SERVICES = [
+  {
+    title: "Real Estate",
+    content: [
+      {
+        name: "Real Estate Sales",
+        ref: "/selling",
+        cover:
+          "https://firebasestorage.googleapis.com/v0/b/varrus-ee20f.appspot.com/o/services%2Fvarrus-properties-realtor-block.png?alt=media&token=8a9a5fbf-096b-44a1-bde5-ff163dfb72f8",
+      },
+      {
+        name: "Property Management",
+        ref: "/leasing",
+        cover:
+          "https://firebasestorage.googleapis.com/v0/b/varrus-ee20f.appspot.com/o/services%2Fvarrus-properties-management-block.png?alt=media&token=80fafa7f-ea41-4de1-9561-ccc6ad88d9eb",
+      },
+    ],
+  },
+  {
+    title: "Media",
+    content: [
+      {
+        name: "Drone Filmography",
+        ref: "/drone",
+        cover:
+          "https://firebasestorage.googleapis.com/v0/b/varrus-ee20f.appspot.com/o/services%2Fvarrus-media-drone-block.png?alt=media&token=39ca4025-6477-4f59-b044-c208ee29702c",
+      },
+      {
+        name: "Virtual Photography",
+        ref: "/virtual",
+        cover:
+          "https://firebasestorage.googleapis.com/v0/b/varrus-ee20f.appspot.com/o/services%2Fvarrus-media-photo-block.png?alt=media&token=0184a882-4dac-4133-9b63-4b695e81820e",
+      },
+    ],
+  },
+  {
+    title: "Construction",
+    content: [
+      {
+        name: "General Construction",
+        ref: "/general",
+        cover:
+          "https://firebasestorage.googleapis.com/v0/b/varrus-ee20f.appspot.com/o/services%2Fvarrus-construction-general-block.png?alt=media&token=76c39f6e-5200-4a27-824b-5266515d3d81",
+      },
+      {
+        name: "Renovations",
+        ref: "/renovations",
+        cover:
+          "https://firebasestorage.googleapis.com/v0/b/varrus-ee20f.appspot.com/o/services%2Fvarrus-construction-renovations-block.png?alt=media&token=cf3d9c83-48cb-4e22-8caf-a3dc43710130",
+      },
+    ],
+  },
+];
 
 export default function Properties({ properties }) {
   const varrusPropertiesLogoLink =
@@ -72,51 +81,38 @@ export default function Properties({ properties }) {
 
   return (
     <>
-      {/* <ChakraImage
-        src={varrusPropertiesLogoLink}
-        width="100vw"
-        height={{ base: "10vh", md: "25vh" }}
-        alt="Properties Logo"
-        borderWidth="1px"
-        rounded="lg"
-        shadow="lg"
-      /> */}
       <Box height="fit-content" overflowY="auto" overflowX="hidden">
         <Text fontSize="2xl" p="4" fontWeight="bold">
-          Properties {/*router.query.purpose*/}
+          Properties
         </Text>
-        <Flex flexWrap="wrap" gridGap={2} p={3}>
+        <Flex
+          flexWrap="wrap"
+          gridGap={2}
+          p={3}
+          justifyContent={{ base: "center", md: "left" }}
+        >
           {properties.map((property) => (
             <Property property={property} key={property.name} />
           ))}
         </Flex>
-
-        <Text fontSize="2xl" p="4" fontWeight="bold">
-          Real Estate
-        </Text>
-        <Flex flexWrap="wrap" gridGap={2} p={3}>
-          {SERVICES.RealEstate.map((service) => (
-            <Service service={service} key={service.name} />
-          ))}
-        </Flex>
-
-        <Text fontSize="2xl" p="4" fontWeight="bold">
-          Media
-        </Text>
-        <Flex flexWrap="wrap" gridGap={2} p={3}>
-          {SERVICES.Media.map((service) => (
-            <Service service={service} key={service.name} />
-          ))}
-        </Flex>
-
-        <Text fontSize="2xl" p="4" fontWeight="bold">
-          Construction
-        </Text>
-        <Flex flexWrap="wrap" gridGap={2} p={3}>
-          {SERVICES.Construction.map((service) => (
-            <Service service={service} key={service.name} />
-          ))}
-        </Flex>
+        {SERVICES.flatMap((service) => (
+          <>
+            <Text fontSize="2xl" p="4" fontWeight="bold" key={service.title}>
+              {service.title}
+            </Text>
+            <Flex
+              flexWrap="wrap"
+              gridGap={2}
+              p={3}
+              display="flex"
+              justifyContent={{ base: "center", md: "left" }}
+            >
+              {service.content.flatMap((content) => (
+                <Service service={content} key={content.name} />
+              ))}
+            </Flex>
+          </>
+        ))}
       </Box>
     </>
   );
